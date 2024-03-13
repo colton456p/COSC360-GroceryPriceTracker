2c6092c4fca809e178eaa406dd90c688d8474868
colton456p <colton.p@hotmail.com>
2024-03-12 19:04:45 -0700

    beginning of sql working

---
+++ account.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Account</title>
+    <link rel="stylesheet" href="css/account.css">
+</head>
+<header>
+    <div id="site_logo">
+        <p>
+            <img id="logo_img" src="img/grocery_logo.png" alt="logo">
+            <h2 >Grocery</h2><h1 class="greentext">Sense</h1>
+        </p>
+    </div>
+    <div id="header-search-div">
+        <form id="search-form" action="">
+            <input type="search" id="search-bar" name="search-bar" placeholder="Search for items...">
+            <input type="submit" value="Search" id="search-button">
+        </form>
+    </div>
+    
+    <div id="menu_bar">
+        <p>
+            <div class="nav-element">
+                <a type="button" id="dashboard-nav-link" href="dashboard.php">DASHBOARD</a>
+            </div>
+            <div class="nav-element">
+                <a type="button" id="trend-nav-link" href="trends.php">TRENDS</a>
+            </div>
+            <div class="nav-element">
+                <a type="button"id="account-nav-link" href="account.php">ACCOUNT</a>
+            </div>
+        </p>
+    </div>
+</header>
+<body>
+    <?php
+        if (isset($_GET['userId'])) {
+            $userId = $_GET['userId'];
+        }
+        $servername = "localhost";
+        $username = "38885190";
+        $dbPass = "38885190";
+        $database = "db_38885190";
+
+        $conn = new mysqli($servername, $username, $dbPass, $database);
+        $sql = "SELECT firstName, lastName, email FROM user WHERE userId =".$userId;
+        $result = $conn->query($sql);
+        $user = $result->fetch_assoc();    
+        $result->close();
+    ?>
+    <div id="container">
+        <h2>Your Account</h2>
+        <h3>User Profile</h3>
+        <div class="indent">
+            <?php
+                echo"<p><strong>Account Number:</strong>".$userId."</p>";
+                echo"<p><strong>First Name:</strong>".$user[0]."</p>";
+                echo"<p><strong>Last Name:</strong>".$user[1]."</p>";
+                echo"<p><strong>Email:</strong>".$user[2]."</p>";
+            ?>
+        </div>
+        <h3>Actions</h3>
+        
+        <a href="#" class="btn">Change Password</a>
+        <a href="homepage.php" style="background-color:rgb(220, 6, 6);"class="btn">Log out</a>
+    </div>
+</body>
+</html>
