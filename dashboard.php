32075d3f792c7b84f3c173dea51b0327b0eae3af
colton456p <colton.p@hotmail.com>
2024-03-14 15:45:52 -0700

    added icons from bootstrap

--- dashboard.php
+++ dashboard.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <link rel="stylesheet" href="css/dashboard.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <script>
             function popUpItem(){
                 
