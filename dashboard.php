26d3ad1503ac95b7883b83eb3ba98a42b30bca12
colton456p <colton.p@hotmail.com>
2024-03-23 15:44:16 -0700

    extra

--- dashboard.php
+++ dashboard.php
@@ -159,6 +159,7 @@
                                     </div>
                                 </div>";
                         }
+                        $results->close();
                         $conn->close();
                     ?>
                 </div>
