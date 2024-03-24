455a4f3da5cf6db01fb208616b721ffe23dc84ca
colton456p <colton.p@hotmail.com>
2024-03-23 19:59:24 -0700

    fix to try and work on content not showing

--- dashboard.php
+++ dashboard.php
@@ -51,22 +51,6 @@
                     }
                 });
             }
-
-            function favourite(productId, storeId) {
-                $.ajax({
-                    url: 'PHP/add_favourites.php',
-                    method: 'POST',
-                    data: {productId: productId, storeId: storeId},
-                    success: function(response) {
-                        console.log('PHP script executed successfully');
-                        console.log('Response:', response);
-                        window.location.reload();
-                    },
-                    error: function(xhr, status, error) {
-                        console.error('Error executing PHP script:', error);
-                    }
-                });
-            }
                 
         </script>
         
@@ -159,34 +143,13 @@
                                     </div>
                                 </div>";
                         }
-                        echo "<h2>TRENDING ITEMS</h2>";
-                        $sql ="SELECT F.itemId, F.productId, G.groceryItemName, G.groceryItemImage, F.storeId FROM favourite AS F JOIN groceryItems AS G ON F.itemId = G.groceryItemId";
-                        $results = $conn->query($sql);
-                        $i = 0;
-                        while($row = $results->fetch_assoc()){
-                            $i+=1;
-                            $productId = $row["productId"];
-                            $itemId = $row["itemId"];
-                            $groceryItemName = $row["groceryItemName"];
-                            $groceryItemImage = $row["groceryItemImage"];
-                            $storeId = $row["storeId"];
-                            $cheapestStore = "Walmart";
-                            echo "<div class=\"item\">
-                                    <div class=\"favourite-icon\" onClick=\"return favourite('".$productId."')\">
-                                        <i class=\"bi-heart-fill\"></i>
-                                    </div>
-                                    <div class =\"item-center-image\" onClick=\"popUpItem('".$productId."', '".$groceryItemName."', '".$groceryItemImage."')\">
-                                        <img id=\"img".$i."\" class=\"item-image\" src=\"".$groceryItemImage."\">
-                                    </div>
-                                    <div class=\"title-click\" onClick=\"popUpItem('".$productId."', '".$groceryItemName."', '".$groceryItemImage."')\">
-                                        <h3 id=\"item".$i."\"class=\"item-name\"> ".$groceryItemName."</h3>
-                                        <h5 class=\"item-price\"><b class=\"greentext\">Lowest price at:</b>".$cheapestStore."</h5>
-                                    </div>
-                                </div>";
-                        }
-
                         $conn->close();
                     ?>
+                </div>
+                <div id="item-shelf-trends">
+                    <div id="trending-title">
+                        <h2>TRENDING ITEMS</h2>
+                    </div>
                 </div>
             </div>
         </div>
