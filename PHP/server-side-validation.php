7dd176f203b7098682791faecd304ebd36782b1c
colton456p <colton.p@hotmail.com>
2024-03-27 21:27:10 -0700

    changed server-side back

--- PHP/server-side-validation.php
+++ PHP/server-side-validation.php
@@ -4,28 +4,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $password = $_POST['password'];
 
     $servername = "localhost";
-    $username = "root";
-    $dbPass = "";
+    $username = "38885190";
+    $dbPass = "38885190";
     $database = "db_38885190";
 
     $conn = new mysqli($servername, $username, $dbPass, $database);
-    $sql = "SELECT userId, adminPriv FROM user WHERE email = '$email' AND pass = '$password'";
+    $sql = "SELECT userId FROM user WHERE email = '$email' AND pass = '$password'";
     if ($conn->connect_error) {
-        header("Location: ../login.php?login=failed");
+        header("Location: ../signup.php?signup=failed");
         exit();
     }
     $result = $conn->query($sql);
     
     if ($result->num_rows < 1) {
-        header("Location: ../login.php?login=failed");
-        exit();
+        $sql = "SELECT adminId FROM adminTable WHERE email = '$email' AND pass = '$password'";
+        $result = $conn->query($sql);
+        $conn->close();
+        if ($result->num_rows < 1) {
+            header("Location: ../login.php?login=failed");
+            exit();
+        }else{
+            $row = $result->fetch_assoc();
+            session_start();
+            $_SESSION["adminId"] = $row["adminId"];
+            header("Location: ../admin.php");
+            exit();
+        }
+        
     }else{
         $row = $result->fetch_assoc();
         session_start();
-        $adminPriv = $row["adminPriv"];
-        if ($adminPriv == 1) {
-            $_SESSION["adminPriv"] = $adminPriv;
-        }
         $_SESSION["userId"] = $row["userId"];
         header("Location: ../dashboard.php");
         exit();
