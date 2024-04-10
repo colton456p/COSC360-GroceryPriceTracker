eef9ce9c90f8fceb5d92e04e58b3ed6a051848ac
colton456p <colton.p@hotmail.com>
2024-04-10 15:51:14 -0700

    db_connect changed

--- PHP/db_connect.php
+++ PHP/db_connect.php
@@ -1,16 +1,16 @@
 <?php
 function db_connect(){
     //For local db connection
-    $servername = "localhost";
-    $username = "webuser";
-    $dbPass = "P@ssw0rd";
-    $database = "db_38885190";
-
-    // //For server db connection
     // $servername = "localhost";
-    // $username = "38885190";
-    // $dbPass = "38885190";
+    // $username = "webuser";
+    // $dbPass = "P@ssw0rd";
     // $database = "db_38885190";
+
+    //For server db connection
+    $servername = "localhost";
+    $username = "38885190";
+    $dbPass = "38885190";
+    $database = "db_38885190";
 
     $conn = new mysqli($servername, $username, $dbPass, $database);
 
